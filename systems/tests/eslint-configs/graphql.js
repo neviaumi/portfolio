48f0f80ca2d75743df1f729b1bf152278c88370a
David Ng <david.ng.dev@gmail.com>
2024-12-21 09:21:36 +0000

    Refactor GraphQL query and extend ESLint config file rules
    Reorganized the GraphQL query structure by adjusting fields for better clarity and consistency, adding `references` while repositioning `title` and `id`. Updated the ESLint configuration to include `.ts` and `.tsx` files for GraphQL linting, improving developer support.

---
+++ systems/tests/eslint-configs/graphql.js
@@ -0,0 +1,55 @@
+import graphqlPlugin from '@graphql-eslint/eslint-plugin';
+
+import pkgjson from '../package.json' with { type: 'json' };
+
+const configName = `${pkgjson.name}/graphql`;
+
+export function useTrustCodeFilesAsGraphQLEslintConfig() {
+  return {
+    files: ['**/*.astro'],
+    name: configName,
+    processor: graphqlPlugin.processor,
+  };
+}
+
+export function useGraphQLEslintConfig() {
+  return {
+    files: ['**/*.graphql'],
+    languageOptions: {
+      parser: graphqlPlugin.parser,
+      parserOptions: {
+        graphQLConfig: {
+          documents: '../cms/tina/__generated__/!(schema).gql',
+          schema: '../cms/tina/__generated__/schema.gql',
+        },
+      },
+    },
+    name: configName,
+    plugins: {
+      '@graphql-eslint': graphqlPlugin,
+    },
+    rules: {
+      ...graphqlPlugin.configs['flat/operations-recommended'].rules,
+      '@graphql-eslint/alphabetize': [
+        'error',
+        {
+          arguments: [
+            'FieldDefinition',
+            'Field',
+            'DirectiveDefinition',
+            'Directive',
+          ],
+          definitions: true,
+          fields: [
+            'ObjectTypeDefinition',
+            'InterfaceTypeDefinition',
+            'InputObjectTypeDefinition',
+          ],
+          selections: ['OperationDefinition', 'FragmentDefinition'],
+          values: true,
+          variables: true,
+        },
+      ],
+    },
+  };
+}
