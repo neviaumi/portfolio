4d9e927591bcb884b27596fd6150c199ecab0233
David Ng <david.ng.dev@gmail.com>
2024-12-27 09:33:00 +0000

    GH-28: Enhance start scripts and clean up docker-compose config
    this part of #28
    Update start scripts to support test mode functionality for CMS and web services. Simplify `docker-compose.yml` by removing unused volumes and adjusting environment variable naming. These changes improve flexibility, maintainability, and environment clarity.

--- systems/cms/scripts/docker/start.sh
+++ systems/cms/scripts/docker/start.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
+
+IS_DEV=${1:---dev}
 
 cp ./vite.config.js ./node_modules/@tinacms/app
-npx tinacms dev
+
+if [ "$IS_DEV" == "--dev" ]; then
+  echo "Start CMS service in dev mode"
+  npx tinacms dev
+elif [ "$IS_DEV" == "--test" ]; then
+  echo "Start CMS service in test mode"
+  npx tinacms dev --noWatch
+else
+    echo "Invalid argument: $IS_DEV"
+    exit 1
+fi
--- systems/web/scripts/docker/start.sh
+++ systems/web/scripts/docker/start.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-npx astro dev --host
+IS_DEV=${1:---dev}
+
+if [ "$IS_DEV" == "--dev" ]; then
+  echo "Start Web frontend in dev mode"
+  npx astro dev --host
+elif [ "$IS_DEV" == "--test" ]; then
+  echo "Start Web frontend in test mode"
+  npx astro build
+  npx astro preview --host
+else
+    echo "Invalid argument: $IS_DEV"
+    exit 1
+fi
