31918a6e96ddea5314c0abae65ac21a62d0d7483
David Ng <david.ng.dev@gmail.com>
2025-01-23 23:32:24 +0000

    GH-57: Set host to true in Vite preview configuration
    this part of #57
    This change enables hosting configuration for Vite's preview server. It ensures the preview server is accessible from external devices, improving testing flexibility.

--- systems/web/astro.config.mjs
+++ systems/web/astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   integrations: [react({ include: ['**/react/*'] })],
   site: 'https://neviaumi.github.io/',
   vite: {
+    preview: {
+      allowedHosts: true,
+    },
     ssr: {
       noExternal: /@mui\/.*?/,
     },
