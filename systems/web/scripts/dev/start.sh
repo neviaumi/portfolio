dab49c58d24e75170d83214f8d0be1e753e65d2a
David Ng <david.ng.dev@gmail.com>
2025-02-11 19:42:31 +0000

    GH-61: Update Astro dependencies and enhance dev mode script
    this close #61
    Upgraded multiple Astro-related dependencies to their latest versions for improved performance and compatibility. Added the `--force` flag to the dev mode script for a more consistent and robust local development experience.

--- systems/web/scripts/dev/start.sh
+++ systems/web/scripts/dev/start.sh
@@ -6,7 +6,7 @@ MODE=${1:---dev}
 export WEB_CMS_BASE_URL=${WEB_CMS_BASE_URL:-"http://localhost:4001"}
 if [ "$MODE" == "--dev" ]; then
   echo "Start Web frontend in dev mode"
-  npx astro dev --host
+  npx astro dev --host --force
 elif [ "$MODE" == "--test" ]; then
   echo "Start Web frontend in test mode"
   npx astro build
