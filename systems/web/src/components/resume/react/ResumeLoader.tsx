9795ff2906463a7ceda1cb2f0ca85c7ab47ecac0
David Ng <david.ng.dev@gmail.com>
2025-02-18 10:48:03 +0000

    GH-113: Add TypeScript error suppression for json-resume component
    this fix #113
    The `@ts-expect-error` directive was added to address the lack of typings for the `json-resume` web component. This prevents TypeScript from raising type errors while the component remains untyped.
    
    Signed-off-by: David Ng <david.ng.dev@gmail.com>

--- systems/web/src/components/resume/react/ResumeLoader.tsx
+++ systems/web/src/components/resume/react/ResumeLoader.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 export default function ResumeLoader() {
   return (
     <Paper>
+      {/*@ts-expect-error No typing for web component*/}
       <json-resume></json-resume>
     </Paper>
   );
