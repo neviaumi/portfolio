b9403898ec10e7930e2a21b684921950fd413501
David Ng <david.ng.dev@gmail.com>
2025-02-27 01:22:32 +0000

    GH-163: Refactor resume handling to support tailored resumes.
    this close #163
    Extracted JSON fetching into a reusable function and updated the LaTeX generation script to dynamically name output files based on tailored resume settings. Adjusted the build script to handle tailored resume configurations and generate appropriately named PDFs.
    
    Signed-off-by: David Ng <david.ng.dev@gmail.com>

--- systems/resume/scripts/dev/build.sh
+++ systems/resume/scripts/dev/build.sh
@@ -2,4 +2,13 @@
 
 set -ex
 
+export VITE_IS_TAILORED_RESUME=$VITE_IS_TAILORED_RESUME
+export VITE_RESUME_SOURCE=$VITE_RESUME_SOURCE
 node ./resume-to-pdf.js
+node ./resume-to-latex.js
+if [ "$VITE_IS_TAILORED_RESUME" = "true" ]; then
+  TAILOR_ID=$(echo "$VITE_RESUME_SOURCE" | awk -F/ '{print $NF}' | sed 's/\.[^.]*$//')
+  (cd public && pdflatex -jobname="ats-$TAILOR_ID" "$TAILOR_ID".tex)
+else
+  (cd public && pdflatex -jobname=ats-resume resume.tex)
+fi
